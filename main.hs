61e8858d1a3db72771b9cd8906954975ec5d1d0e
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-06-09 16:49:21 +0300

    finish next chapter or something

--- main.hs
+++ main.hs
@@ -1,8 +1,20 @@
-main = putStrLn (wrapHtml "My simple webpage" "Hello, World!")
+main = putStrLn (makeHtml "My simple webpage" (h1_ "Hello, world!" <> p_ "This is a simple webpage."))
 
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
-head_ content = "<head>" <> content <> "</head>"
-title_ content = "<title>" <> content <> "</title>"
+html_ :: String -> String
+html_ = el "html"
+body_ :: String -> String
+body_ = el "body"
+head_ :: String -> String
+head_ = el "head"
+title_ :: String -> String
+title_ = el "title"
+p_ :: String -> String
+p_ = el "p"
+h1_ :: String -> String
+h1_ = el "h1"
 
-wrapHtml title body = html_ (head_ (title_ title) <> body_ body)
+makeHtml :: String -> String -> String
+makeHtml title body = html_ (head_ (title_ title) <> body_ body)
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
