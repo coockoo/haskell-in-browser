c1d571633ddfb51a6946fb02795a5bc2c5e652ce
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-06-16 17:45:55 +0300

    add working traffic lights

--- app/Sokoban.hs
+++ app/Sokoban.hs
@@ -1,22 +1,37 @@
 import CodeWorld
 
 topCircle :: Color -> Picture
-topCircle c = translated 0 1.5 $ colored c $ solidCircle 1
+topCircle c = translated 0 2.5 $ colored c $ solidCircle 1
+
+midCircle :: Color -> Picture
+midCircle c = colored c $ solidCircle 1
 
 botCircle :: Color -> Picture
-botCircle c = translated 0 (-1.5) $ colored c $ solidCircle 1
+botCircle c = translated 0 (-2.5) $ colored c $ solidCircle 1
 
 frame :: Picture
-frame = rectangle 2.5 5.5
+frame = rectangle 2.5 7.5
 
-trafficLight :: Bool -> Picture
-trafficLight True = topCircle black & botCircle green & frame
-trafficLight False = topCircle red & botCircle black & frame
+data State = Go | PreStop | Stop | PreGo
 
-trafficController :: Double -> Picture
-trafficController t
-  | even $ round (t / 3) = trafficLight True
-  | otherwise          = trafficLight False
+lights :: State -> Picture
+lights Go = topCircle black & midCircle black & botCircle green
+lights PreStop = topCircle black & midCircle yellow & botCircle black
+lights Stop = topCircle red & midCircle black & botCircle black
+lights PreGo = topCircle red & midCircle yellow & botCircle black
+
+controller :: State -> Picture
+controller state = lights state & frame
+
+animatedController :: Double -> Picture
+animatedController t = case round t `mod` 6 of
+  0 -> controller Go
+  1 -> controller Go
+  2 -> controller PreStop
+  3 -> controller Stop
+  4 -> controller Stop
+  5 -> controller PreGo
+  _ -> blank
 
 main :: IO ()
-main = animationOf trafficController
+main = animationOf animatedController
