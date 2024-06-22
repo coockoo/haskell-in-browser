8804d7507807d8a54c1823e732cd25dd16dc27e1
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-06-22 10:26:47 +0300

    make tree abstract

--- app/Sokoban.hs
+++ app/Sokoban.hs
@@ -1,36 +1,4 @@
 import CodeWorld
 
-topCircle :: Color -> Picture
-topCircle c = translated 0 2.5 $ colored c $ solidCircle 1
-
-midCircle :: Color -> Picture
-midCircle c = colored c $ solidCircle 1
-
-botCircle :: Color -> Picture
-botCircle c = translated 0 (-2.5) $ colored c $ solidCircle 1
-
-frame :: Picture
-frame = rectangle 2.5 7.5
-
-data State = Go | PreStop | Stop | PreGo
-
-lights :: State -> Picture
-lights Go = topCircle black & midCircle black & botCircle green
-lights PreStop = topCircle black & midCircle yellow & botCircle black
-lights Stop = topCircle red & midCircle black & botCircle black
-lights PreGo = topCircle red & midCircle yellow & botCircle black
-
-controller :: State -> Picture
-controller state = lights state & frame
-
-animatedController :: Double -> Picture
-animatedController t
-  | n == 0 || n == 1 = controller Go
-  | n == 2           = controller PreStop
-  | n == 3 || n == 4 = controller Stop
-  | n == 5           = controller PreGo
-  | otherwise        = blank
-  where n = (round t `mod` 6) :: Integer
-
 main :: IO ()
-main = animationOf animatedController
+main = drawingOf blank
--- app/Tree.hs
+++ app/Tree.hs
@@ -1,12 +1,15 @@
 import CodeWorld
 
 flower :: Double -> Picture
-flower t = colored yellow $ solidCircle (min (t/20) 0.3)
+flower t = colored yellow $ solidCircle (min (t/33) 0.3)
 
-tree :: Integer -> Double -> Picture
-tree 0 t = flower t
-tree n t = polyline [(0,0),(0,1)] & translated 0 1 (
-  rotated (pi/10) (tree (n-1) t) & rotated (-pi/10) (tree (n-1) t))
+tree :: Integer -> Picture -> Picture
+tree 0 pic = pic
+tree n pic = polyline [(0,0),(0,1)] & translated 0 1 (
+  rotated (pi/10) (tree (n-1) pic) & rotated (-pi/10) (tree (n-1) pic))
+
+bloom :: Double -> Picture
+bloom t = tree 8 $ flower t
 
 main :: IO ()
-main = animationOf $ tree 5
+main = animationOf bloom
