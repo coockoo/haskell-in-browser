42602ca9db606c7ab5f6ba3f15175335b48b27f2
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-07-18 22:33:34 +0300

    safeString

--- homework-5/Lists.hs
+++ homework-5/Lists.hs
@@ -1,8 +1,17 @@
+import Data.Char (isAscii)
+
 halveEvens :: [Integer] -> [Integer]
 halveEvens [] = []
 halveEvens (x : xs)
   | even x = (x `div` 2) : halveEvens xs
   | otherwise = halveEvens xs
 
+safeString :: String -> String
+safeString [] = []
+safeString (x : xs)
+  | isAscii x = x : safeString xs
+  | otherwise = '_' : safeString xs
+
 main :: IO()
-main = print $ halveEvens [6,6,6,3,3,3,2,2,2]
+main = print $ safeString "🙋.o"
+-- main = print $ halveEvens [6,6,6,3,3,3,2,2,2]
