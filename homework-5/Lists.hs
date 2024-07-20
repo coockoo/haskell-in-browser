2743d5f3ea0c043433f899db4cc9390882f51355
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-07-20 22:11:49 +0300

    add holes solution

--- homework-5/Lists.hs
+++ homework-5/Lists.hs
@@ -1,4 +1,5 @@
 import Data.Char (isAscii)
+import Data.Semigroup (Semigroup(stimes))
 
 halveEvens :: [Integer] -> [Integer]
 halveEvens = foldr (\x acc -> if even x then (x `div` 2) : acc else acc) []
@@ -6,6 +7,15 @@ halveEvens = foldr (\x acc -> if even x then (x `div` 2) : acc else acc) []
 safeString :: String -> String
 safeString = foldr (\x acc -> (if isAscii x then x else '_') : acc) ""
 
+holes :: [a] -> [[a]]
+holes list = map doDrop (zip [0..] clone)
+ where
+   clone = stimes (length list) [list]
+   doDrop (i, l) = take i l ++ drop (i+1) l
+
 main :: IO()
-main = print $ safeString "🙋.o"
--- main = print $ halveEvens [6,6,6,3,3,3,2,2,2]
+main =
+  putStr "Homework 5: Exercise 1: Lists, lists, lists"
+  <> putStr "halveEvens: " <> print (halveEvens [6,6,6,3,3,3,2,2,2])
+  <> putStr "safeString: " <> print (safeString "🙋.o")
+  <> putStr "holes: " <> print (holes "Hello")
