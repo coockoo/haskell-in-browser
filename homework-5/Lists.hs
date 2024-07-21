271a425e20c300f4e26437be9e357f6ab8e145ec
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-07-21 18:16:15 +0300

    finish homework with lists

--- homework-5/Lists.hs
+++ homework-5/Lists.hs
@@ -1,7 +1,10 @@
-import Data.Char (isAscii)
+import Data.Char (isAscii, isNumber, digitToInt)
 import Data.Semigroup (Semigroup(stimes))
 import Data.List (maximumBy)
 import Data.Function (on)
+
+-- challenge for this homework is not to use recursive functions directly,
+-- only list stuff
 
 halveEvens :: [Integer] -> [Integer]
 halveEvens = foldr (\x acc -> if even x then (x `div` 2) : acc else acc) []
@@ -10,20 +13,37 @@ safeString :: String -> String
 safeString = map (\x -> (if isAscii x then x else '_'))
 
 holes :: [a] -> [[a]]
-holes list = zipWith (curry doDrop) [0..] clone
+holes list = zipWith doDrop [0..] clone
  where
    clone = stimes (length list) [list]
-   doDrop (i, l) = take i l ++ drop (i+1) l
+   doDrop i l = take i l ++ drop (i+1) l
 
 longestText :: Show a => [a] -> a
 longestText = maximumBy (compare `on` length . show)
 
 adjacents :: [a] -> [(a,a)]
-adjacents list = zipWith (curry go) [0 .. ] clone
+adjacents list = zipWith go [0..] clone
   where
     clone = stimes (length list - 1) [list]
     firstAndLast l = (head l, last l)
-    go = firstAndLast . take 2 . uncurry drop
+    go i l = firstAndLast $ take 2 $ drop i l
+
+commas :: [String] -> String
+commas list = unwords $ zipWith go [0 ..] list
+  where
+    go i l = if i == length list - 1 then l else l ++ ","
+
+addPolynomials :: [[Integer]] -> [Integer]
+addPolynomials list = foldr (zipWith (+)) (head list) (drop 1 list)
+
+sumNumbers :: String -> Integer
+sumNumbers str = res
+  where
+    (res, _) = foldr go (0, 1) str
+    go :: Char -> (Integer, Integer) -> (Integer, Integer)
+    go el (acc, times) = if isNumber el
+                         then (acc + times * toInteger (digitToInt el), times * 10)
+                         else (acc, 1)
 
 main :: IO()
 main =
@@ -33,3 +53,7 @@ main =
   <> putStr "holes: " <> print (holes "Hello")
   <> putStr "longestText: " <> print (longestText [2::Int,4,16,32])
   <> putStr "adjacents: " <> print (adjacents "Hello")
+  <> putStr "commas: " <> print (commas ["Hello", "World", "Kappa"])
+  <> putStr "addPolynomials: " <> print (addPolynomials [[0, 1, 5], [7, 0, 0], [-2, -1, 5]])
+  <> putStr "sumNumbers: " <> print (sumNumbers "words0are1234separated12by3integers45678")
+
