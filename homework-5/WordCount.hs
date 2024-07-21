c7d6606681cf227ea9d3999d6f6011b3076f1186
Oleksandr Bondarchuk <alex.coockoo@gmail.com>
2024-07-21 21:49:32 +0300

    followers

--- homework-5/WordCount.hs
+++ homework-5/WordCount.hs
@@ -1,5 +1,13 @@
 import Data.List (nub, maximumBy)
 import Data.Function (on)
+import Data.Semigroup (Semigroup(stimes))
+
+adjacents :: [a] -> [(a,a)]
+adjacents list = zipWith go [0..] clone
+  where
+    clone = stimes (length list - 1) [list]
+    firstAndLast l = (head l, last l)
+    go i l = firstAndLast $ take 2 $ drop i l
 
 wordCount :: String -> String
 wordCount str =
@@ -7,14 +15,19 @@ wordCount str =
   "Number of empty lines: " ++ numberOfEmptyLines ++ "\n" ++
   "Number of words: " ++ numberOfWords ++ "\n" ++
   "Number of unique words: " ++ numberOfUniqueWords ++ "\n" ++
-  "Number of words followed by themselves: idk, probably some\n" ++
+  "Number of words followed by themselves: " ++ numberOfFollowers ++ "\n" ++
   "Length of the longest line: " ++ lline ++ "\n"
   where
     numberOfLines = show (length (lines str))
     numberOfEmptyLines = show (length (filter null (lines str)))
     numberOfWords = show (length (words str))
     numberOfUniqueWords = show (length (nub (words str)))
+    numberOfFollowers = show $
+      foldr
+      (\(a, b) acc -> if a == b then acc + 1 else acc)
+      (0 :: Integer)
+      (adjacents (words str))
     lline = show (length (maximumBy (compare `on` length) (lines str)))
 
 main :: IO()
-main = putStr (wordCount "123\n4567\n123\n")
+main = putStr (wordCount "123\n4567\n123\n123\n")
